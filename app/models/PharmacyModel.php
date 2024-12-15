e6e65cb5f1bf7436913097a99ffb0dc79aa00a3b
Sulakshana Karunarathne <sula2001karunarathne@gmail.com>
2024-12-15 22:48:15 +0530

    wip: pharmacy reviews

--- app/models/PharmacyModel.php
+++ app/models/PharmacyModel.php
@@ -1,5 +1,8 @@
 <?php 
 
+require_once '../app/core/Model.php';
+require_once '../app/core/Database.php';
+
 class PharmacyModel
 {
 	
@@ -7,17 +10,49 @@ class PharmacyModel
 
 	protected $table = 'pharmacy';
 
-	protected $allowedColumns = [
+        public function getPharmacy()
+        {
+                $query = "SELECT
+                            pharmacy_id,
+                            name,
+                            CONCAT(district, ', ', city) as location,
+                            contact_no
+                         FROM $this->table
+                         ORDER BY pharmacy_id DESC";
+                
+                return $this->query($query);
 
-	        'user_id',
-            'name',
-            'district',
-            'city',
-            'street',
-            'contact_no',
-            'certificate'
-	];
+        }
 
+        public function searchPharmacy($name = '', $location = '')
+       {
+                $params = [];
+                $query = "SELECT
+                            pharmacy_id,
+                            name,
+                            CONCAT(district, ', ', city) as location,
+                            contact_no
+                         
+                 FROM $this->table 
+                 WHERE 1=1";
+
+        if (!empty($name)) {
+           $query .= " AND LOWER(name) LIKE :name";
+           $params['name'] = '%' . strtolower($name) . '%';
+        }
+    
+
+        if (!empty($location)) {
+            $query .= " AND (LOWER(district) LIKE :location OR LOWER(city) LIKE :location)";
+            $params['location'] = '%' . strtolower($location) . '%';
+        }
+
+        $query .= " ORDER BY pharmacy_id DESC";
+        
+        return $this->query($query, $params);
+    }
+
+	
 
 	
 }
